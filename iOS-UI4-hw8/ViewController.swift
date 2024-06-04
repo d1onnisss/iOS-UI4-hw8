2f111aad240bb86fbd5472c8d9040d060b6c6e63
d1onnisss <lim_al@auca.kg>
2024-06-04 16:14:12 +0600

    Initial Commit

---
+++ iOS-UI4-hw8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-UI4-hw8
+//
+//  Created by Alexey Lim on 4/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
